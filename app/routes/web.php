b0fcf79ea1102f58894fd5e8c947012b1716c2b1
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-09-25 09:46:58 -0300

    Feat: InfTasksStatus routes

--- app/routes/web.php
+++ app/routes/web.php
@@ -2,17 +2,15 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+
+$router->group(['prefix' => 'status'], function () use ($router) {
+    $router->get('', 'InfTasksStatusController@getAllStatus');
+    $router->post('', 'InfTasksStatusController@createStatus');
+    $router->get('{uuid}', 'InfTasksStatusController@getStatusByUuid');
+    $router->put('{uuid}', 'InfTasksStatusController@updateStatus');
+    $router->delete('{uuid}', 'InfTasksStatusController@deleteStatus');
 });
