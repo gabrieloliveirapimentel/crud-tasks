5b2d5c89b2862c525508424210afa9288da60c87
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-09-25 11:07:38 -0300

    Feat: Tasks routes

--- app/routes/web.php
+++ app/routes/web.php
@@ -14,3 +14,11 @@ $router->group(['prefix' => 'status'], function () use ($router) {
     $router->put('{uuid}', 'InfTasksStatusController@updateStatus');
     $router->delete('{uuid}', 'InfTasksStatusController@deleteStatus');
 });
+
+$router->group(['prefix' => 'tasks'], function () use ($router) {
+    $router->get('', 'TasksController@getAllTasks');
+    $router->post('', 'TasksController@createTask');
+    $router->get('{uuid}', 'TasksController@getTasksByUuid');
+    $router->put('{uuid}', 'TasksController@updateTask');
+    $router->delete('{uuid}', 'TasksController@deleteTask');
+});
